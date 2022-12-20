46beaf8c1a53922fac39f40764efa9759b02de10
Rafiya-Rehan21 <rafiya.21rehan@gmail.com>
2022-12-20 12:32:39 +0500

    class assignment

---
+++ projectheader.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main ()
+{ 
+cout << "***********************************************************************" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                       RAF-21 LIMIITED                               *" << endl;
+cout << "*                     Deals in INNOVATION                             *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "***********************************************************************" << endl;
+}
